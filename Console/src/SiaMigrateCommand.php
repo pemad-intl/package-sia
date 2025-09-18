0d7e76131a78444250a6f004eabe8f189fa69f2b
Backend2 <agustinus2h@gmail.com>
2025-09-18 14:14:34 +0700

    urutan migrasi

--- Console/src/SiaMigrateCommand.php
+++ Console/src/SiaMigrateCommand.php
@@ -15,15 +15,35 @@ class SiaMigrateCommand extends Command
         $vendorPath = base_path('vendor/digipemad/sia');
         $modules = File::directories($vendorPath); 
 
-        foreach ($modules as $modulePath) {
+        // Tentukan urutan modul
+        $moduleOrder = [
+            'Academic',
+            'Administration',
+            'Boarding',
+            'Counseling',
+            'Teacher'
+        ];
+
+        // Buat array baru dengan modul sesuai urutan
+        $orderedModules = [];
+        foreach ($moduleOrder as $modName) {
+            foreach ($modules as $modulePath) {
+                if (str_contains($modulePath, $modName)) {
+                    $orderedModules[] = $modulePath;
+                }
+            }
+        }
+
+        foreach ($orderedModules as $modulePath) {
             $migrationsPath = $modulePath . '/src/Database/Migrations';
             if (File::exists($migrationsPath)) {
                 $relativePath = str_replace(base_path() . DIRECTORY_SEPARATOR, '', $migrationsPath);
-                $relativePath = str_replace('\\', '/', $relativePath); // <-- FIX
+                $relativePath = str_replace('\\', '/', $relativePath);
                 
                 $this->info("Migrating: $modulePath");
                 $this->call('migrate', [
                     '--path' => $relativePath,
+                    '--database' => 'pgsql', 
                     '--force' => $this->option('force')
                 ]);
             }
